e42d7611cfa3b6cb9af44af7e1204f383d013c7e
Liu Yongsheng <liuxiaosheng.boy@hotmail.com>
2014-08-19 10:14:24 +0800

    use new plugin for watch-reload-open process

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,41 @@
 var gulp = require('gulp'),
     open = require("gulp-open"),
-    livereload = require('gulp-livereload');
+    connect = require('gulp-connect');
 
 var dest = "dist",
     port = 8080,
+    watchPath = dest + "/**", //监测的文件路径
     openPath = dest + "/index.html", //用浏览器打开的文件路径
     openOption = {
         url: "http://127.0.0.1:" + port
     };
-//在dest目录下启动静态文件服务器
-gulp.task('server', function(next) {
-  var connect = require('connect'),
-      server = connect();
-  server.use(connect.static(dest)).listen(process.env.PORT || port, next);
-});
 
 //用浏览器打开dest目录下的index.html文件
-gulp.task('open', ['server'], function(){
+gulp.task('open', /*['server'],*/ function(){
   gulp.src(openPath)
       .pipe(open("", openOption));
 });
 
-//监测dest目录本地文件改动，并重新打开dest目录下的index.html
-gulp.task('watch', ['server'], function() {
-  var server = livereload();
-  gulp.watch(dest + '/**').on('change', function(file) {
-      server.changed(file.path);
-
-      gulp.src(openPath)
-          .pipe(open("", openOption));
+/*
+  在dist目录下启动静态文件服务器
+  插件doc: https://github.com/AveVlad/gulp-connect
+*/
+gulp.task('connect', function() {
+  connect.server({
+    port:port,
+    root: dest,
+    directoryListing: true,
+    livereload: true
   });
 });
 
-gulp.task('default', ['server', 'open', 'watch']);
+gulp.task('reload-html', function () {
+  gulp.src(watchPath)
+    .pipe(connect.reload());
+});
+//监测dist目录中的变动，并触发reload
+gulp.task('connect-watch', function () {
+  gulp.watch([watchPath], ['reload-html']);
+});
+
+gulp.task('default', ['connect', 'open', 'connect-watch']);
