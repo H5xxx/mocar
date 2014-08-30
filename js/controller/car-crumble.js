6e552c148e0c2be958ecc11bab0ad3ab5b05744e
尹锋 <ingf.ivan@gmail.com>
2014-08-30 09:56:26 +0800

    new api doc

---
+++ js/controller/car-crumble.js
@@ -0,0 +1,14 @@
+define(function(require, exports) {
+    var Transitions = require('../component/transitions');
+    var Brand = require('../model/brand');
+
+    var CarBrand = Spine.Controller.create({
+        init: function() {},
+        nav: function(data) {
+            var page = data.page;
+            var previewValue = data.previewValue;
+            var previewDom = 
+        },
+        back: function() {}
+    });
+});
