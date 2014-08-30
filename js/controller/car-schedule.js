32cb17c6fed896a5bf9af1e352e45313d70d6bc5
Liu Yongsheng <liuxiaosheng.boy@hotmail.com>
2014-08-30 12:17:16 +0800

    单页里面初始化弹窗/CustomSelect

--- js/controller/car-schedule.js
+++ js/controller/car-schedule.js
@@ -4,6 +4,7 @@
 define(function(require, exports) {
     var Brand = require('../model/brand');
     var Model = require('../model/model');
+    var CustomSelect = require('../widgets/CustomSelect');
 
     var CarSchedule = require('./common').sub({
         // 该controller要渲染&控制的区域
@@ -26,8 +27,71 @@ define(function(require, exports) {
 
             this.el.html(html);
             //TODO，初始化自定义select
+            setTimeout(initCustomSelect, 200)
         },
     });
+    function initCustomSelect(){
+        initSelect();
+        function initSelect () {
+            var optArrs = [
+                [
+                    ['北京市'],
+                    ['上海市'],
+                    ['广州市'],
+                    ['天津市']
+                ],
+                [
+                    ['东城区'],
+                    ['朝阳区'],
+                    ['海淀区'],
+                    ['西城区']
+                ],
+                [
+                    ['2014-07-22'],
+                    ['2014-07-23'],
+                    ['2014-07-24'],
+                    ['2014-07-25'],
+                    ['2014-07-26'],
+                    ['2014-07-27'],
+                    ['2014-07-28']
+                ],
+                [
+                    ['08:00'],
+                    ['09:00'],
+                    ['10:00'],
+                    ['11:00'],
+                    ['12:00'],
+                    ['13:00'],
+                    ['14:00'],
+                    ['15:00'],
+                    ['16:00'],
+                    ['17:00']
+                ]
+            ];
+            var selectWrappers = document.querySelectorAll('.select-wrapper');
+            var selectWrapper, selectTrigger, selectInput;
 
+            for(var i = 0, ilen = selectWrappers.length; i < ilen; i++){
+                selectWrapper = selectWrappers[i];
+                selectTrigger = selectWrapper.querySelector('.custom-select-trigger');
+                selectInput = selectWrapper.querySelector('input');
+                
+                if(selectTrigger && selectInput){
+                    (function(selectTrigger, selectInput, optArr){
+                        new CustomSelect(selectTrigger, selectInput, optArr,  function onchange(selectedIndex){
+                            if(selectedIndex !== -1){
+                                var productNameEl = selectTrigger.querySelector('.product-name');
+                                var priceEl = selectTrigger.querySelector('.price');
+                                productNameEl.innerHTML = optArr[selectedIndex][0];
+                                if(priceEl){
+                                    priceEl.innerHTML = optArr[selectedIndex][1];
+                                }
+                            }
+                        });
+                    })(selectTrigger, selectInput, optArrs[i])
+                }
+            };
+        }
+    }
     return CarSchedule;
 });
