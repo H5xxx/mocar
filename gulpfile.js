ddcc5b3b4f60fffcd662abdf9af7ec3d5d9919a1
Liu Yongsheng <liuxiaosheng.boy@hotmail.com>
2014-09-14 22:46:46 +0800

    gulpfile.js中添加合并css/js逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
     open = require("gulp-open"),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    seajs = require('gulp-seajs'),
+    del = require('del'),
+    copy = require('gulp-copy'),
+    combineCSS = require('combine-css'),
+    concat = require('gulp-concat'),
+    htmlreplace = require('gulp-html-replace');;
 
 var dest = __dirname, //本地开发时的监测目录，部署时用dist目录
+    releaseDest = "dist/",
     port = 8088,
     watchPath = [ //监测的文件路径
         dest + "/html/**/*.html", 
@@ -10,14 +17,18 @@ var dest = __dirname, //本地开发时的监测目录，部署时用dist目录
         dest + "/css/**/*.css"
     ],
     openPath = dest + "/html/index.html", //用浏览器打开的文件路径
+    distOpenPath = releaseDest + "/html/index.html", //用浏览器打开的文件路径
     openOption = {
         url: "http://127.0.0.1:" + port + "/html/index.html?code=FAKECODE#/"
     },
     openOption2 = {
         url: "http://127.0.0.1:" + port + "/html/index.html?code=FAKECODE#/service/1/cart"
+    },
+    distOpenOption = {
+        url: "http://127.0.0.1:" + port + "/html/index.html?code=FAKECODE#/"    
     };
 
-//用浏览器打开dest目录下的index.html文件
+//用浏览器打开html目录下的index.html文件
 gulp.task('open', /*['server'],*/ function(){
     gulp.src(openPath)
         .pipe(open("", openOption));
@@ -26,7 +37,7 @@ gulp.task('open', /*['server'],*/ function(){
 });
 
 /*
-  在dist目录下启动静态文件服务器
+  在project根目录下启动静态文件服务器
   插件doc: https://github.com/AveVlad/gulp-connect
 */
 gulp.task('connect', function() {
@@ -37,7 +48,19 @@ gulp.task('connect', function() {
         livereload: true
     });
 });
-
+/*
+  在dist目录下启动静态文件服务器
+  插件doc: https://github.com/AveVlad/gulp-connect
+*/
+gulp.task('connect-dist', ['relocateHtml'], function(cb) {
+    connect.server({
+        port:port,
+        root: releaseDest,
+        directoryListing: true,
+        livereload: true
+    });
+    cb();
+});
 gulp.task('reload-html', function () {
     gulp.src(watchPath)
         .pipe(connect.reload());
@@ -47,4 +70,71 @@ gulp.task('connect-watch', function () {
     gulp.watch([watchPath], ['reload-html']);
 });
 
+gulp.task('clean', function(cb){
+    del(['dist/**/*'], function (err) {
+        if(err){
+            console.error(err);
+        }else{
+            console.log('Files deleted'); 
+        }
+        cb(err);
+    });
+});
+//cmd transport & concat
+gulp.task('concat', ['clean'], function(cb){
+    //把js/lib/目录下的所有js 按顺序合并到一个大文件中
+    gulp.src([
+            'js/lib/iScroll-v4.2.5.js',
+            'js/lib/template.js',
+            'js/lib/sea.js',
+            'js/lib/zepto.js',
+            'js/lib/spine.js',
+            'js/lib/route.js',
+            'js/lib/route.js',
+            'js/lib/manager.js'
+            ])
+        .pipe(concat('libAllInOne.js', {newLine: ';'}))
+        .pipe(gulp.dest('dist/js/lib/'));
+    //把所有的业务代码合并到一个大文件中
+    gulp.src('js/page/index.js')
+        .pipe( seajs('page/index') )
+        .pipe( gulp.dest( 'dist/js/page/' ) );
+    cb();
+});
+
+//combine css
+gulp.task('combineCSS', function(cb) {
+    //TODO 目前只是对css文件做简单的拼接合并，以后用专门的css 合并工具
+    gulp.src([
+            'css/base.css',
+            'css/home.css',
+            'css/service.css',
+            'css/cart.css',
+            'css/schedule.css',
+            'css/success.css',
+            ])
+        .pipe(concat('cssAllInOne.css'))
+        .pipe(gulp.dest('dist/css/'));
+    cb();
+});
+//使用合并之后的js/css
+gulp.task('relocateHtml', ['concat', 'combineCSS'], function(cb){
+    gulp.src('html/index.html')
+        .pipe(htmlreplace({
+            'css': '../css/cssAllInOne.css',
+            'js': '../js/lib/libAllInOne.js'
+        }))
+        .pipe(gulp.dest('dist/html/'));
+
+    // gulp.src("html/index.html")
+    //     .pipe(copy("dist/"));
+    // gulp.src("js/lib/*.js")
+    //     .pipe(copy("dist/"));
+    gulp.src("css/icons/*.*")
+        .pipe(copy("dist/"));
+    cb();
+});
+
 gulp.task('default', ['connect', 'open', 'connect-watch']);
+
+gulp.task('combine', ['clean', 'concat', 'combineCSS' , 'relocateHtml', "connect-dist", "open"]);
